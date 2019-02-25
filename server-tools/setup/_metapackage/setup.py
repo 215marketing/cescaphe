9e343691cec80813977b7a911296e536db232693
215 Marketing <garrett@215marketing.com>
2019-02-25 18:20:20 -0500

    add server tools final 1.4

---
+++ server-tools/setup/_metapackage/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo12-addons-oca-server-tools",
+    description="Meta package for oca-server-tools Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo12-addon-base_search_fuzzy',
+        'odoo12-addon-html_image_url_extractor',
+        'odoo12-addon-html_text',
+        'odoo12-addon-module_auto_update',
+        'odoo12-addon-sentry',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)

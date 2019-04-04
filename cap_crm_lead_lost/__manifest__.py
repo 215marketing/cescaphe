d0c991ea72080ab44c171d5bf9d2cb12d879514a
captivea-jpa <35844968+captivea-jpa@users.noreply.github.com>
2019-04-04 21:59:51 +0200

    Create __manifest__.py

---
+++ cap_crm_lead_lost/__manifest__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+# Part of CAPTIVEA. Odoo 12 EE.
+
+{
+    'name': "cap_crm_lead_lost",
+    'version': "1.0",
+    'author': "captivea-jpa",
+    'summary': "Manage 'crm.lead.lost' default Odoo model.",
+    'depends': ["crm"],
+    'installable': True
+}

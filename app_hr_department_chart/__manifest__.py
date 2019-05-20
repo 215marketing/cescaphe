9e76a5872a531c61d33ce44bb1d804e451a87343
NathanQj <nathan.qjm@gmail.com>
2019-05-20 14:56:09 -0400

    Add files via upload

--- app_hr_department_chart/__manifest__.py
+++ app_hr_department_chart/__manifest__.py
@@ -5,22 +5,6 @@
 # email: 300883@qq.com
 # resource of Sunpop
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
-
-# Odoo12在线用户手册（长期更新）
-# https://www.sunpop.cn/documentation/user/12.0/en/index.html
-
-# Odoo12在线开发者手册（长期更新）
-# https://www.sunpop.cn/documentation/12.0/index.html
-
-# Odoo10在线中文用户手册（长期更新）
-# https://www.sunpop.cn/documentation/user/10.0/zh_CN/index.html
-
-# Odoo10离线中文用户手册下载
-# https://www.sunpop.cn/odoo10_user_manual_document_offline/
-# Odoo10离线开发手册下载-含python教程，jquery参考，Jinja2模板，PostgresSQL参考（odoo开发必备）
-# https://www.sunpop.cn/odoo10_developer_document_offline/
-
-
 {
     'name': 'Hr Department Chart Hierarchy, 员工部门多层级结构图',
     'version': '12.19.04.19',
@@ -33,7 +17,7 @@
     Chart Hierarchy Widget. Hierarchy Chart, Hierarchy Tree for multi level Parent Children relation tree.
     Free for category Hierarchy chart, stock Hierarchy chart. account chart.
     """,
-    'description': """    
+    'description': """
 This module extend to show a Hierarchy chart.
 (N+1, N+2, direct subordinates)
 image: image_field,
@@ -67,4 +51,3 @@ child_all_count: child_all_count field, count of direct and indirect children.
     'application': True,
     'auto_install': False,
 }
-
